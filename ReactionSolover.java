a47a37036187396cec56b7513de07bad450b5a8e
Ivan Gromakovsky <gie1994@gmail.com>
2015-01-29 00:20:15 +0300

    Update ReactionSolover

--- ReactionSolover.java
+++ ReactionSolover.java
@@ -11,13 +11,6 @@ public class ReactionSolover {
      */
     public static PhysicalValue[] getVariables() {
         return new PhysicalValue[]{
-                // These are left for sample
-                new PhysicalValue("a", 0.007),
-                new PhysicalValue("b", 2),
-                new PhysicalValue("c", 3),
-                new PhysicalValue("time", 1000, "sec"),
-                new PhysicalValue("length", 1000, "px"),
-
                 // Definitely constants
                 new PhysicalValue("E",      8 * 1e4,            "J/mol"),
                 new PhysicalValue("R",      8.314,              "J/(mol * K)"),
@@ -31,9 +24,11 @@ public class ReactionSolover {
                 // May be changed
                 new PhysicalValue("K",      1.6 * 1e-6,         "1/sec"),
                 new PhysicalValue("D",      0.13 / 830 / 1980,  "m^2 / sec"),    // initially: lambda / rho / c
-                new PhysicalValue("dz",     1e-4,               "m"),
+                new PhysicalValue("Tw",     300,                "K"),
                 new PhysicalValue("dt",     1e-4,               "sec"),
+                new PhysicalValue("dz",     1e-4,               "m"),
                 new PhysicalValue("l",      1,                  "m"),
+                new PhysicalValue("steps",  1000),
         };
     }
 
@@ -51,26 +46,42 @@ public class ReactionSolover {
     T_i^{n+1} - T_i^{n} = dt * (lambda / (rho * c) * (T^{n+1}_{i+1} - 2 * T^n_{i} + T^n_{i-1}) / dz^2 - K * Q / c * (x_i^{n+1})^alpha * exp(-E / (R * T_i^n))
      */
     public static Graph[] solove(PhysicalValue[] physicalValues) {
-        double a = findValue(physicalValues, "a").value;
-        double b = findValue(physicalValues, "b").value;
-        double c = findValue(physicalValues, "c").value;
-        int n = findValue(physicalValues, "time").intValue();
-        int m = findValue(physicalValues, "length").intValue();
+        double D = findValue(physicalValues, "D").value;
+        double E = findValue(physicalValues, "E").value;
+        double R = findValue(physicalValues, "R").value;
+        double alpha = findValue(physicalValues, "alpha").value;
+        double K = findValue(physicalValues, "K").value;
+        double T0 = findValue(physicalValues, "T0").value;
+        double Tw = findValue(physicalValues, "Tw").value;
 
-        double[][][] d = new double[3][n][m];
+        double dt = findValue(physicalValues, "dt").value;
+        double dz = findValue(physicalValues, "dz").value;
+        double l = findValue(physicalValues, "l").value;
+        int z_steps = (int) Math.round(l / dz);
+        int t_steps = findValue(physicalValues, "steps").intValue();
 
-        for (int t = 0; t < n; t++) {
-            for (int x = 0; x < m; x++) {
-                d[0][t][x] = sin(a * x + b * t);
-                d[1][t][x] = cos(a * x + b * t + c * x * t);
-                d[2][t][x] = d[0][t][x] + d[1][t][x];
-            }
+        double[][][] d = new double[3][t_steps][z_steps];
+        double[][] X = d[0];
+        double[][] T = d[1];
+        double[][] W = d[2];
+
+        for (int i = 0; i < z_steps; i++) {
+            X[0][i] = i == 0 ? 1 : 0;
+            T[0][i] = i == 0 ? Tw : T0;
+            W[0][i] = K * Math.pow(X[0][i], alpha) * Math.exp(-E / (R * T[0][i]));
         }
 
+//        for (int t = 0; t < n; t++) {
+//            for (int x = 0; x < m; x++) {
+//                d[0][t][x] = sin(a * x + b * t);
+//                d[1][t][x] = cos(a * x + b * t + c * x * t);
+//            }
+//        }
+
         return new Graph[]{
-                new Graph(d[0], "sin"),
-                new Graph(d[1], "cos"),
-                new Graph(d[2], "sin + cos"),
+                new Graph(X, "X"),
+                new Graph(T, "T"),
+                new Graph(W, "W"),
         };
     }
 
